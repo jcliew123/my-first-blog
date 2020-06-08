b88dc5b97505f7c8f7718a5e150f363500c6d7a4
Jc <jcliew123@gmail.com>
2020-06-08 19:35:46 +0800

    Added an online CV page

--- blog/views.py
+++ blog/views.py
@@ -18,15 +18,16 @@ def myself(request):
 @login_required
 def post_new(request):
     if request.method == "POST":
+        form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=false)
+            post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form' : form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
